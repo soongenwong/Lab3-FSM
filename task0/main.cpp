e5d8395aac214ebfe71996e042c8576c6d23606a
Soong En Wong <wsoongen2015@gmail.com>
2024-11-01 11:01:30 +0000

    completed task0

--- task0/main.cpp
+++ task0/main.cpp
@@ -23,9 +23,28 @@ TEST_F(TestAdd, AddTest)
     EXPECT_EQ(add(2, 4), 6);
 }
 
-TEST_F(TestAdd, AddTest2)
+TEST_F(TestAdd, AddNegativeNumbers)
 {
     // Create a test case here. Maybe fail this to see what happens?
+    EXPECT_EQ(add(-2, -3), -5 );
+}
+
+TEST_F(TestAdd, AddWithZero)
+{
+    EXPECT_EQ(add(0, 5), 5);
+    EXPECT_EQ(add(5, 0), 5);
+}
+
+TEST_F(TestAdd, FailingTest)
+{
+    EXPECT_EQ(add(2, 2), 4);
+}
+
+TEST_F(TestAdd, DifferentAssertations)
+{
+    ASSERT_EQ(add(3, 4), 7);     // Hard assertion - stops the test if it fails
+    EXPECT_GT(add(5, 5), 9);     // Expects result to be greater than 9
+    EXPECT_LT(add(1, 1), 3);     // Expects result to be less than 3
 }
 
 int main(int argc, char **argv)
--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -1,8 +1,15 @@
 module lfsr(
     input   logic       clk,
     input   logic       rst,
-    input   logic       en,
-    output  logic [3:0] data_out
+    output  logic [4:1] data_out
 );
+
+always_ff @ (posedge clk, posedge rst)
+    if(rst)
+        sreg <= 4'b1;
+    else 
+        sreg <= {sreg[3:1], sreg[4], ^ sreg[3]};
+
+assign data_out = sreg;
 
 endmodule
